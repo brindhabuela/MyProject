ac384e8e65af5a2d28f56a8bdffd35c57e992666
Brindha <brindhabuela@users.noreply.github.com>
2016-08-22 16:02:40 +0530

    Add files via upload

---
+++ Reverse.java
@@ -0,0 +1,13 @@
+package logical.Ms1;
+
+public class Reverse 
+{
+public static void main(String[] args) 
+{
+int[] a=new int[]{1,3,5,4,2};
+String str=a.toString();
+StringBuffer sb=new StringBuffer(str);
+System.out.println(sb.reverse());
+
+}
+}
